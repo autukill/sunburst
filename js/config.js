4b2dc0e33e4e09aea438119a52438b1599c8f08a
Unknown <393382242@qq.com>
2018-03-23 21:39:07 +0800

    新增: config 文件和题库
    题库文件字符串编码必须是 utf-8

---
+++ js/config.js
@@ -0,0 +1,76 @@
+window.sunburst = window.sunburst ? window.sunburst : {};
+
+window.sunburst.config = {
+	"color":[
+		"",
+		"orange",
+		"cyan",
+		"green",
+		"blue",
+		"red"
+	],
+	"papers": {
+		"v2018": {
+			"subjects": [{
+					"dirName": "0",
+					"name": "电力安全工作规程",
+					"papers": [{
+							"name": "卷一",
+							"fileName": "1.txt"
+						},
+						{
+							"name": "卷二",
+							"fileName": "2.txt"
+						},
+						{
+							"name": "卷三",
+							"fileName": "3.txt"
+						},
+						{
+							"name": "卷四",
+							"fileName": "4.txt"
+						},
+						{
+							"name": "卷五",
+							"fileName": "5.txt"
+						},
+						{
+							"name": "卷六",
+							"fileName": "6.txt"
+						}
+					]
+				},
+				{
+					"dirName": 0,
+					"name": "安全生产令",
+					"papers": [{
+							"name": "卷一",
+							"fileName": "1.txt"
+						},
+						{
+							"name": "卷二",
+							"fileName": "2.txt"
+						},
+						{
+							"name": "卷三",
+							"fileName": "3.txt"
+						},
+						{
+							"name": "卷四",
+							"fileName": "4.txt"
+						},
+						{
+							"name": "卷五",
+							"fileName": "5.txt"
+						},
+						{
+							"name": "卷六",
+							"fileName": "6.txt"
+						}
+					]
+				}
+			]
+		}
+	}
+
+}
