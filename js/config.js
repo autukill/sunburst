eedc3d3ed522324c7b418a65b233be12094f2127
YVYHN <360367789@qq.com>
2020-02-27 20:52:30 +0800

    update js/config.js. 增加科目13

--- js/config.js
+++ js/config.js
@@ -2,6 +2,6 @@ window.sunburst = window.sunburst ? window.sunburst : {};
 
 window.sunburst.config = {
 	"papers": {
-		"subjects": ["科目1", "科目3", "十个规定动作"]
+		"subjects": ["科目1", "科目3","科目13", "十个规定动作"]
 	}
 }
