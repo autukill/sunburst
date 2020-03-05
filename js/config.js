41b561c6808d21765372fd2b24a48846127429fe
YVYHN <360367789@qq.com>
2020-03-05 21:27:08 +0800

    update js/config.js. 新增科目22

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window.sunburst = window.sunburst ? window.sunburst : {};
 
 // 防止缓存 txt
-window._version = "2020-03-03"
+window._version = "2020-03-05"
 
 window.sunburst.config = {
 	"papers": {
-		"subjects": ["科目1", "科目3","科目13", "十个规定动作"]
+		"subjects": ["科目1", "科目3","科目13","科目22", "十个规定动作"]
 	}
 }
